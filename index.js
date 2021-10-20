90bfb83df4f2f13cdc08711105e8a0817b2ee9cf
Okoth Pius <orukopius8@gmail.com>
2021-10-20 17:23:00 +0300

    feat: update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const PORT = 8000
+const PORT = process.env.PORT || 8000
 const cheerio = require('cheerio');
 const axios = require('axios');
 const express = require('express')
@@ -7,20 +7,70 @@ const app = express() //Initialise
 
 const newspapers = [
   {
-    name: 'thetimes',
-    address: 'https://www.thetimes.co.uk/environment/climate-change',
-    base: ''
-  },
-  {
-    name: 'guardian',
-    address: 'https://www.theguardian.com/environment/climate-crisis',
-    base: ''
-  },
-  {
-    name: 'telegraph',
-    address: 'https://www.telegraph.co.uk/climate-change',
-    base: 'https://www.telegraph.co.uk/'
-  }
+        name: 'cityam',
+        address: 'https://www.cityam.com/london-must-become-a-world-leader-on-climate-change-action/',
+        base: ''
+    },
+    {
+        name: 'thetimes',
+        address: 'https://www.thetimes.co.uk/environment/climate-change',
+        base: ''
+    },
+    {
+        name: 'guardian',
+        address: 'https://www.theguardian.com/environment/climate-crisis',
+        base: '',
+    },
+    {
+        name: 'telegraph',
+        address: 'https://www.telegraph.co.uk/climate-change',
+        base: 'https://www.telegraph.co.uk',
+    },
+    {
+        name: 'nyt',
+        address: 'https://www.nytimes.com/international/section/climate',
+        base: '',
+    },
+    {
+        name: 'latimes',
+        address: 'https://www.latimes.com/environment',
+        base: '',
+    },
+    {
+        name: 'smh',
+        address: 'https://www.smh.com.au/environment/climate-change',
+        base: 'https://www.smh.com.au',
+    },
+    {
+        name: 'un',
+        address: 'https://www.un.org/climatechange',
+        base: '',
+    },
+    {
+        name: 'bbc',
+        address: 'https://www.bbc.co.uk/news/science_and_environment',
+        base: 'https://www.bbc.co.uk',
+    },
+    {
+        name: 'es',
+        address: 'https://www.standard.co.uk/topic/climate-change',
+        base: 'https://www.standard.co.uk'
+    },
+    {
+        name: 'sun',
+        address: 'https://www.thesun.co.uk/topic/climate-change-environment/',
+        base: ''
+    },
+    {
+        name: 'dm',
+        address: 'https://www.dailymail.co.uk/news/climate_change_global_warming/index.html',
+        base: ''
+    },
+    {
+        name: 'nyp',
+        address: 'https://nypost.com/tag/climate-change/',
+        base: ''
+    }
 ]
 
 articles = []
@@ -51,6 +101,32 @@ app.get('/', (req, res) => {
 
 app.get('/news', (req, res) => {
   res.json(articles)
+})
+
+app.get('/news/:newspaperId', (req, res) => {
+  const newspaperId = req.params.newspaperId
+
+  const newspaperAddress = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].address
+  const newspaperBase = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].base
+
+  axios.get(newspaperAddress)
+  .then(response => {
+    const html = response.data
+    const $ = cheerio.load(html)
+    const specificArticles = []
+
+    $('a:contains("climate")', html).each( function () {
+      const title = $(this).text()
+      const url = $(this).attr('href')
+      specificArticles.push({
+        title,
+        url: newspaperBase + url,
+        source: newspaperId
+      })
+    })
+    res.json(specificArticles)
+
+  }).catch(err => console.log(err))
 })
 
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`))
