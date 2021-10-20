2b7d84c2dea814b39c3266d51cee79c0297b39b2
Okoth Pius <orukopius8@gmail.com>
2021-10-20 15:24:53 +0300

    feat: getting data from multiple sources

--- index.js
+++ index.js
@@ -5,29 +5,52 @@ const express = require('express')
 
 const app = express() //Initialise
 
+const newspapers = [
+  {
+    name: 'thetimes',
+    address: 'https://www.thetimes.co.uk/environment/climate-change',
+    base: ''
+  },
+  {
+    name: 'guardian',
+    address: 'https://www.theguardian.com/environment/climate-crisis',
+    base: ''
+  },
+  {
+    name: 'telegraph',
+    address: 'https://www.telegraph.co.uk/climate-change',
+    base: 'https://www.telegraph.co.uk/'
+  }
+]
+
 articles = []
+
+newspapers.forEach(newspaper => {
+  axios.get(newspaper.address)
+  .then(response => {
+    const html = response.data
+    const $ = cheerio.load(html)
+
+    $('a:contains("climate")', html).each(function () {
+      const title = $(this).text()
+      const url = $(this).attr('href')
+
+      articles.push({
+        title,
+        url: newspaper.base + url,
+        source: newspaper.name,
+      })
+    })
+  })
+});
+
 //Routes
 app.get('/', (req, res) => {
   res.json('Welcome to my Climate Change News API')
 })
 
 app.get('/news', (req, res) => {
-  axios.get('https://www.theguardian.com/environment/climate-crisis')
-  .then((response) => {
-    const html = response.data
-    const $ = cheerio.load(html)
-
-    $('a:contains("climate")', html).each(function() {
-      const title = $(this).text()
-      const url = $(this).attr('href')
-
-      articles.push({
-        title,
-        url
-      })
-    })
-    res.json(articles)
-  }).catch((err) => console.log(err))
+  res.json(articles)
 })
 
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`))
